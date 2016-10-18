6ddd86d1ca8c9e764174d1b501a4e1bbbbaaf070
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-10-18 07:36:24 -0700

    revised some exercises

--- README.md
+++ README.md
@@ -10,13 +10,17 @@ Reference [this page](https://learn.jquery.com/using-jquery-core/document-ready/
 
 Go to the [MDN docs for selectors](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_started/Selectors).  Read through the docs to get an idea of how selectors work.  Next, look at the [jQuery page on selectors](https://learn.jquery.com/using-jquery-core/selecting-elements/).
 
-1. Find the secretBox on the page. Set the background color to white.  Add an h1 tag that says, "secret box!"
-2. Find all child divs of the first row.  Set the class for each div to boxType3.
-3. Make the last box in the last row disappear. (Hint, look into the dispaly style.  Also, you should only get back one element from your selector.).
+1. Find the secretBox on the page (there is more than one way to select this element. Challenge yourself to use a couple different methods!). Set the background color to white.  Add an h1 tag that says, "secret box!"
+2. Find all child divs of the first row. Print to the console the number of items returned from this query, and set the class for each div to boxType3. Then, check to make sure that the class was successfully added.
+3. Make the last box in the last row disappear. (Hint, look into the display style.  Also, you should only get back one element from your selector.).
 4. Change all red boxes to white.
 5. Get the first two divs in the second row.  Take away all styling from the divs.
 6. Get all divs inside the container that are not row divs and are not the secret box div.  Set the width of the divs to 2 pixels.
 
 ### Stretch Exercises
 
-//Apply some kind of animation to the secretBox. The animation should take two seconds to complete, and once it has finished, the only element the user should see is an h1 element with the text "Animation complete!".
+1. Find all divs that have a parent div with a class of "row". Console.log() the boolean "true" if the div has a class of "boxType1", and "false" if the div has a class of "boxType2" or "boxType2".
+
+1. 7. Using the toggle() method, make the document body disappear. Add one line of code to make it reappear. Can you explain how this works? Then, make the body toggle out quickly and then back into view slowly by adding arguments to the toggle methods.
+
+1. Apply some kind of animation to the secretBox. The animation should take two seconds to complete, and once it has finished, the only element the user should see is an h1 element with the text "Animation complete!".
--- boxes.js
+++ boxes.js
@@ -7,30 +7,33 @@
 //Method #1
 $( document ).ready(function() {
     //console.log( "ready for DOM manipulation!" );
+    //alert('ready for DOM manipulation!')
 });
 
 //Method #2
 $(function() {
     //console.log( "this is the shorthand version" );
+    //alert('ready for DOM manipulation!')
 });
 
 //Method #3
 function readyFn() {
     // Code to run when the document is ready.
     //console.log("named function version");
+    //alert('ready for DOM manipulation!')
 }
 $( document ).ready( readyFn );
 
 
 // ******* EXERCISE 2 *******
-// Find the secretBox on the page. Set the background color to white. Add an h1 tag that says, "secret box!"
+// Find the secretBox on the page (there is more than one way to select this element. Challenge yourself to use a couple different methods!). Set the background color to white. Add an h1 tag that says, "secret box!"
 
 $( document ).ready(function() {
     // var secretBox = $("#secretBox");
     // secretBox.css('background-color', 'white').html("<h1>secret box!</h1>");
 });
 
-// Find all child divs of the first row. Set the class for each div to boxType3.
+// Find all child divs of the first row. Print to the console the number of items returned from this query, and set the class for each div to boxType3. Then, check to make sure that the class was successfully added.
 
 $( document ).ready(function() {
 
@@ -38,12 +41,14 @@ $( document ).ready(function() {
     //$("#row1").children().addClass('boxType3');
 
     //longhand
-    // var firstRow = $("#row1");
-    // var kiddos = firstRow.children();
-    // kiddos.addClass('boxType3');
+    var firstRow = $("#row1");
+    var kiddos = firstRow.children();
+    console.log(kiddos.length);
+    kiddos.addClass('boxType3');
 
-    //STRETCH: REMOVE CLASSES BOXTYPE1 & BOXTYPE2 BEFORE ADDING BOXTYPE3
-
+    for (var i = 0; i < kiddos.length; i++){
+      console.log(kiddos[i].className);
+    }
 
 });
 
@@ -71,26 +76,47 @@ $( document ).ready(function() {
 
 // Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
 
-$( document ).ready(function() {
-  $('.box').not('#secretBox').width('2px');
-});
+// $( document ).ready(function() {
+//   $('.box').not('#secretBox').width('2px');
+// });
 
 
 
 
 // ****** EXTRA TASKS ********
 
-//Apply some kind of animation to the secretBox. The animation should take two seconds to complete, and once it has finished, the only element the user should see is an h1 element with the text "Animation complete!".
 
-$( document ).ready(function() {
-  $('.box').not('#secretBox').width('2px');
-
-  $( "#secretBox" ).animate({
-    opacity: 0.25,
-    left: "+=100",
-    height: "toggle"
-  }, 2000, function() {
-    alert('animation complete!');
+//***Find all divs that have a parent div with a class of "row". Console.log() the boolean "true" if the div has a class of "boxType1", and "false" if the div has a class of "boxType2" or "boxType2".
+  $( document ).ready(function() {
+    //console.log($( "#container" ).children().size());
   });
 
-});
+
+  //$( "li" ).has( "ul" ).css( "background-color", "red" );
+
+//Using the toggle() method, make the document body disappear. Add one line of code to make it reappear. Can you explain how this works? Then, make the body toggle out quickly and then back into view slowly by adding arguments to the toggle methods.
+// $( document ).ready(function() {
+//   $( "body" ).toggle('fast');
+//   $( "body" ).toggle('slow');
+// });
+
+//***On page load, make divs with a class of boxType1 invisible (different than display: none).Make divs with a class of boxType2 fade out one second after the document is read. Make the boxType1 divs fade into view immediately following the boxType2 fadeout (use 'complete' function).
+
+
+
+//Apply some kind of animation to the secretBox. The animation should take two seconds to complete, and once it has finished, the only element the user should see is an h1 element with the text "Animation complete!".
+
+// $( document ).ready(function() {
+//   $('.box').not('#secretBox').width('2px');
+//
+//   $( "#secretBox" ).animate({
+//     opacity: 0.25,
+//     left: "+=100",
+//     height: "toggle"
+//   }, 2000, function() {
+//     //remove the #container div
+       // append an h1 to the body
+       //center the h1
+//   });
+//
+// });
